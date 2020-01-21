0ea0438b7e6fb3da0ca2a88cb768562ece0cb836
Chip <lefrostx@gmail.com>
2020-01-21 16:04:09 +0500

    Add Photo Form class

---
+++ models/PhotoForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class PhotoForm extends Model
+{
+    public $file;
+    public $link;
+    public $info;
+
+    public function rules()
+    {
+        return
+            [
+                [['file'], 'file', 'extensions'='jpg,png',
+                    'message'=>'Фото должно быть формата jpg или png',
+                    'skipOnEmpty'=>false],
+                [['link', 'info'], 'string'],
+        ];
+    }
+
+    public function attributeLabels()
+    {
+        return
+            [
+                'file' => 'Загрузить изображение',
+                'link' => 'Ссылка',
+                'info' => 'Информация',
+        ];
+    }
+}
